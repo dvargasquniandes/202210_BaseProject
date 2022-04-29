ea8243dfad406a316b4a8f1f40ddd734a80ce0c6
David <david.vargas@decameron.com>
2022-04-29 10:22:06 -0500

    feat: Create vehicle list

---
+++ src/app/vehicle/vehicle-list/vehicle-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Vehicle } from '../vehicle';
+import { VehicleService } from '../vehicle.service';
+
+
+@Component({
+  selector: 'app-vehicle-list',
+  templateUrl: './vehicle-list.component.html',
+  styleUrls: ['./vehicle-list.component.css']
+})
+export class VehicleListComponent implements OnInit {
+
+  vehicles:Array<Vehicle> = []
+
+  constructor(private vehicleService: VehicleService) { }
+
+  getMuseums():void{
+    this.vehicleService.getVehicles().subscribe((vehicles)=>{
+      this.vehicles = vehicles;
+    })
+  }
+
+  ngOnInit(): void {
+    this.getMuseums();
+  }
+
+}
